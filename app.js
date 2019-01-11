68b7d8c01e95b2533bed8f0c9a951ea904cd9132
colinskeep <colinjinskeep@gmail.com>
2019-01-11 15:08:53 -0700

    various changes

--- app.js
+++ app.js
@@ -2,17 +2,27 @@ const { Chromeless } = require('chromeless');
 require('dotenv').config();
 
 async function login(){
-  const chromeless = new Chromeless();
+  const chromeless = new Chromeless({
+    scrollBeforeClick: false,
+    debug: true,
+    implicitWait: true
+  });
 
   const screenshot = await chromeless
     .goto('https://accounts.google.com/signin/v2/identifier?hl=en&continue=https%3A%2F%2Fwww.google.com%2F&flowName=GlifWebSignIn&flowEntry=AddSession')
     .type(process.env.USER_EMAIL, '.whsOnd.zHQkBf')
     .click('div[id="identifierNext"]')
+    .wait('div[id="password"]')
     .type(process.env.USER_PW, '.whsOnd.zHQkBf')
     .click('div[id="passwordNext"]')
     .wait('div[class="ds"]')
-    .screenshot()
-
+    .goto('https://www.youtube.com/')
+    .goto('https://www.youtube.com/watch?v=urtiMOp6BTU')
+    .wait('button[data-post-action="/service_ajax?name=likeEndpoint"]')
+    .wait(1000)
+    .mousedown('button[data-post-action="/service_ajax?name=likeEndpoint"]')
+    .mouseup('button[data-post-action="/service_ajax?name=likeEndpoint"]')
+    .click('button[data-post-action="/service_ajax?name=likeEndpoint"]')
   await chromeless.end()
 }
 
