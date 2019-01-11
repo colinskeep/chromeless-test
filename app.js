fbb4eef877d424e30921bc949d1c5a98c41552d9
colinskeep <colinjinskeep@gmail.com>
2019-01-11 09:42:11 -0700

    google login

---
+++ app.js
@@ -0,0 +1,19 @@
+const { Chromeless } = require('chromeless');
+require('dotenv').config();
+
+async function login(){
+  const chromeless = new Chromeless();
+
+  const screenshot = await chromeless
+    .goto('https://accounts.google.com/signin/v2/identifier?hl=en&continue=https%3A%2F%2Fwww.google.com%2F&flowName=GlifWebSignIn&flowEntry=AddSession')
+    .type(process.env.USER_EMAIL, '.whsOnd.zHQkBf')
+    .click('div[id="identifierNext"]')
+    .type(process.env.USER_PW, '.whsOnd.zHQkBf')
+    .click('div[id="passwordNext"]')
+    .wait('.status')
+    .screenshot()
+
+  await chromless.end()
+}
+
+login().catch(console.error.bind(console))
